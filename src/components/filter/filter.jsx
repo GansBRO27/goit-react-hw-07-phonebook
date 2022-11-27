e6eef8dcfd7cf00b15303808ba369157b9944af9
GansBRO27 <vladyslavgdjmg@gmail.com>
2022-11-27 20:51:19 +0200

    qweqwe

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,16 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { filterContacts } from 'redux/contactSlise';
+import { filterContacts } from '../../redux/contactSlise';
 
 export default function Filter() {
-  // const filter = useSelector(state => state.contacts.filter);
+  const filter = useSelector(state => state.filter);
+  console.log(filter);
   const dispatch = useDispatch();
   const handleChangeFilter = e => {
-    // dispatch(filterContacts(e.currentTarget.value));
+    dispatch(filterContacts(e.currentTarget.value));
   };
   return (
     <input
       onChange={handleChangeFilter}
-      // value={filter}
+      value={filter}
       type="text"
       name="filter"
       pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
